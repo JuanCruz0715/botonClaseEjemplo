e615a68c25ed699f8521de0b8b42daf26f90101f
JuanCruz0715 <juanchi97cruz@gmail.com>
2023-08-15 12:17:58 -0300

    v.full

---
+++ src/Componentes/Boton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Boton.css';
+
+const nombre = 'Botoncito';
+
+const Boton = () => {
+  return (
+    <div className="Boton" >
+      {' '}{nombre}
+    </div>
+  );
+}
+
+export default Boton;
